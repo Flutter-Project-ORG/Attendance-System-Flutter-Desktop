ebeaf4c0dfe1da3bf0b7350ddbfab60488433b3c
Osama <osama.assaf.o@gmail.com>
2022-11-02 20:07:46 +0300

    commit

---
+++ lib/res/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+
+  static const Color darkBgColor = Color(0xFF191919);
+  static const Color darkPrimaryColor = Color(0xFFC84B31);
+  static const Color darkSecondaryColor = Color(0xFF2D4263);
+  static const Color darkTextColor = Color(0xFFECDBBA);
+
+
+  static const Color lightBgColor = Color(0xFFF7F7F7);
+  static const Color lightPrimaryColor = Color(0xFFFFE61B);
+  static const Color lightSecondaryColor = Color(0xFFFFB72B);
+  static const Color lightTextColor = Color(0xFF191919);
+
+}
