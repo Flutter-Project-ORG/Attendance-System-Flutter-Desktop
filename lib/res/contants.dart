6e19a5cae8363afb159979346f3ccf1d9eaa2e8a
Osama <osama.assaf.o@gmail.com>
2022-11-03 07:45:58 +0300

    commit

---
+++ lib/res/contants.dart
@@ -0,0 +1,5 @@
+class Constants{
+
+  static const String apiKey = "AIzaSyB8v1L7QXGHY5mm-N-Sfc0r5TM__aT9bOU";
+
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,6 +5,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
